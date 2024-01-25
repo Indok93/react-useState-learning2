3453bcdd483216b96b9ddafb4056efe249b12af4
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-01-25 09:31:59 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import { Home } from './pages/Home/Home'
+import { Header } from './components/Header/Header'
+import { Footer } from './components/Footer/Footer'
 import { Counter } from './components/Counter/Counter'
 import { Message } from './components/Message/Message'
 import { ToDoList } from './components/ToDoList/ToDoList'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
 
 function App() {
 
